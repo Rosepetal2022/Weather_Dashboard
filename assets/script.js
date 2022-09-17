e570ab296a1c7850c67a7e09e24c94cb4d02a42c
Rachel <marcotter25@yahoo.com>
2022-09-17 08:23:10 -0700

    updated fetch request and weather cards

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,16 @@
 
 let userInput = document.querySelector(".input");
 let searchForm = document.querySelector("#search-form");
-let mainWeather = document.querySelector('.main-weather');
-let weatherStats = document.querySelector('.weather-stats')
+let cityName = document.querySelector('#city-name');
+let weatherStats = document.querySelector('.weather-stats');
+let city = userInput.value
+
+
 
 
 //get Lat and Lon for the current weather api call
 let getLatLon = (city) => {
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=35364985ed49d890b2571e2cb173bbb0`)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=35364985ed49d890b2571e2cb173bbb0`)
     .then(response => {
         return response.json()
     })
@@ -22,9 +25,9 @@ let getLatLon = (city) => {
 let getCurrentWeather = (data) => {
     let cityLat = data.city.coord.lat
     let cityLon = data.city.coord.lon
-  
-    
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=35364985ed49d890b2571e2cb173bbb0`)
+
+    //fetch statement for current weather
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&units=imperial&appid=35364985ed49d890b2571e2cb173bbb0`)
     .then(response => {
         if (response.ok) {
         return response.json()
@@ -34,33 +37,133 @@ let getCurrentWeather = (data) => {
 })
     .then(data => {
         console.log(data)
-        //create El li's that list the weather data we want to display 
-        mainWeather.innerHTML = data.name 
-        
-        
+        cityName.textContent = data.name
 
+        //let search = document.querySelector('.search-history')
         
+        //let searchHistory = JSON.parse(localStorage.getItem('city'));
         
-        //data.weather[0].main
+        //cityArr.push(searchHistory);
 
-    })
+
+        //Populate the data to the li in the index.HTML
+        let dateTime = document.querySelector('#date-time')
+        dateTime.setAttribute('class', 'date-time')
+        dateTime.textContent = moment().format("MMM Do YYYY");
+
+        let weather = document.querySelector('.description')
+        weather.textContent = ('Conditions: ' +  data.weather[0].description)
+
+        let temp = document.querySelector('.temp')
+        temp.textContent = ('Temperature: ' + data.main.temp + ' Degrees')
+        
+        let windSpeed = document.querySelector(".wind-speed")
+        windSpeed.textContent = ('Wind Speed: ' + data.wind.speed + ' mph');
+
+        let humidity = document.querySelector('.humidity')
+        humidity.textContent = ('Humidity: ' + data.main.humidity + ' %');
+
+
+        let icon = document.querySelector('.icon')
+        let weatherIcon = document.createElement('img')
+        weatherIcon.setAttribute('src', "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+        icon.appendChild(weatherIcon)
+
+
+        fiveDayForecast(cityLat, cityLon);
+        
+})
     .catch((error) => console.error("Fetch Error" , error))
+}    
 
-   
+//function to get the data for five day forcast as well as populating the data to cards
+//and putting it on the page.
+let fiveDayForecast = (cityLat, cityLon) => {
+     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&units=imperial&appid=35364985ed49d890b2571e2cb173bbb0`)
+     .then(response => {
+         if (response.ok) {
+         return response.json()
+     } else {
+         alert("City Not Found")
+     }
+ })
+     .then(data => {
+         console.log(data)
+
+         let card = document.querySelector('.five-day-container')
     
-    
-    console.log(mainWeather)
-    
+         for(i=0;i<5;i++) {
+            let cardInfo = document.createElement('div')
+            cardInfo.classList.add('card', 'bg-primary', 'text-white', 'rounded', 'mr-2', 'flex-fill');
+
+           
+            let iconEl = document.createElement('img')
+            let fiveDayDate = document.createElement('p')
+            let tempEl = document.createElement('p')
+            let humidityEl = document.createElement('p')
+            let windEl = document.createElement('p')
+            
+            
+            let startDate = moment().add(1, 'day').add(i, 'day').format('MMM Do YYYY')
+            let cardTemp = (data.list[i].main.temp + ' degrees')
+            let cardHumidity = ('humidity ' + data.list[i].main.humidity + ' %')
+            let cardWind = ('wind ' + data.list[i].wind.speed + ' mph')
+            
+            iconEl.setAttribute('src', "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+            
+        
+            
+            fiveDayDate.textContent = startDate
+            tempEl.textContent = cardTemp
+            humidityEl.textContent = cardHumidity
+            windEl.textContent = cardWind
+            
+            cardInfo.appendChild(iconEl)
+            card.appendChild(cardInfo)
+            cardInfo.appendChild(fiveDayDate)
+            cardInfo.appendChild(tempEl)
+            cardInfo.appendChild(humidityEl)
+            cardInfo.appendChild(windEl)
+            
+           
+        } 
+    })
+     .catch((error) => console.error("Fetch Error" , error))
 }
 
 
+//design for the input button
+let inputButton = document.querySelector('#input-button');
+inputButton.classList.add('card', 'bg-primary', 'text-white', 'rounded', 'mr-2', 'flex-fill')
+
+//function for setting and getting local storage
+let setCity = () => {
+    let cityArr =[];
+
+cityArr.push(userInput.value)
+localStorage.setItem('cityArr', JSON.stringify(cityArr));
 
 
-//save search history to local storage
-/*window.localStorage.setItem('userInput')
+let searchHistory = document.querySelector('#history')
+console.log(searchHistory)
 
-//load the anything in local storage to the page
-window.localStorage.getItem('userInput')*/
+for(i=0; i < cityArr.length; i++) {
+
+    const cityName = cityArr[i] 
+
+
+    let historyEl = document.createElement('p')
+    historyEl.textContent = cityName
+    historyEl.classList.add('card', 'bg-primary', 'text-white', 'rounded', 'mr-2', 'flex-fill')
+
+
+    searchHistory.appendChild(historyEl)
+
+    console.log(cityName)
+};
+
+};
+
 
 
 //function to handle the click event
@@ -68,8 +171,8 @@ clickEventHandler = (e) => {
     e.preventDefault();
     let city = userInput.value.trim();
     getLatLon(city);
+    setCity(city);
 };
-
 
 
 //event listeners for user input
